d8ac6517b826c30fdb6eab3a94d10303f0f28f5f
Nwaigba1234567 <iamkatty3@gmail.com>
2023-08-23 15:36:19 -0700

    Added db_storage.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""Defines the State class."""
+
+import models
+from os import getenv
+from models.base_model import Base, BaseModel
+from models.city import City, Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """a state for a MySQL database"""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
