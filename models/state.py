636a56efeae5c34e16ca7243ce1135e0eabd06d5
Nwaigba1234567 <iamkatty3@gmail.com>
2023-09-24 16:37:45 -0700

    Added mprove engines

--- models/state.py
+++ models/state.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
+
 
 class State(BaseModel, Base):
-    """a state for a MySQL database"""
-
+    """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    name = Column(String(128), nullable=False)
+
+    # relationship to get all cities of this state saved in db
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
+
+    elif getenv("HBNB_TYPE_STORAGE") == "fs":
         @property
         def cities(self):
-            city_list = []
-            for city in list(models.storage.all(City).values()):
+            """Getter for cities of this state saved in FileStorage"""
+            cities = []
+            # filter for all cities that are in FileStorage
+            for city in models.storage.all(City).values():
+                # grab only cities that have this as their state
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    cities.append(city)
+            return cities
